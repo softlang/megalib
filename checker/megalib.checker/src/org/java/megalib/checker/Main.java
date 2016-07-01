f0e4471f2cf5f7c4508f2ea3577910cc09a97815
Merlin May <masteraudio.123@web.de>
2016-07-01 08:00:36 +0200

    [megalib] new project, ant build file generated

---
+++ checker/megalib.checker/src/org/java/megalib/checker/Main.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.java.megalib.checker;
+
+/**
+ * @author mmay, aemmerichs
+ *
+ */
+public class Main {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hallo Welt!");
+	}
+}
