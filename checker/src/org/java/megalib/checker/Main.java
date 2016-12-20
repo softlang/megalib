6eb698c67665771435607f7e0b0402dec26b003f
Marcel Heinz <heinz@uni-koblenz.de>
2016-12-20 12:13:49 +0100

    Fixed bug at relation instances using turtle syntax Improved model

--- checker/src/org/java/megalib/checker/Main.java
+++ checker/src/org/java/megalib/checker/Main.java
@@ -11,82 +11,84 @@ import org.java.megalib.checker.services.MegaModelLoader;
 /**
  * @author mmay@uni-koblenz.de, aemmerichs@uni-koblenz.de
  * @author heinz
- *
  */
 public class Main {
 
     public static void main(String[] args) {
-	try {
-	    if (validArguments(args)) {
-		File f = new File(args[1]);
-		if (!f.exists()) {
-		    System.err.println("The file '" + args[1] + "' does not exist.");
-		    return;
-		}
+        try {
+            if (validArguments(args)) {
+                File f = new File(args[1]);
+                if (!f.exists()) {
+                    System.err.println("The file '" + args[1] + "' does not exist.");
+                    return;
+                }
 
-		if (!args[1].endsWith(".megal")) {
-		    if (f.isDirectory()) {
-			if (args.length > 2 && args[2].equals("-nocon")) {
-			    checkDirectory(f.getAbsolutePath(), true);
-			} else {
-			    checkDirectory(f.getAbsolutePath(), false);
-			}
-		    } else {
-			System.err.println("Could not recognize '" + args[1] + "' as a megamodel.");
-		    }
-		} else {
-		    if (args.length > 2 && args[2].equals("-nocon")) {
-			checkFile(f.getAbsolutePath(), true);
-		    } else {
-			checkFile(f.getAbsolutePath(), false);
-		    }
-		}
+                if (!args[1].endsWith(".megal")) {
+                    if (f.isDirectory()) {
+                        if (args.length > 2 && args[2].equals("-nocon")) {
+                            checkDirectory(f.getAbsolutePath(), true);
+                        } else {
+                            checkDirectory(f.getAbsolutePath(), false);
+                        }
+                    } else {
+                        System.err.println("Could not recognize '" + args[1] + "' as a megamodel.");
+                    }
+                } else {
+                    if (args.length > 2 && args[2].equals("-nocon")) {
+                        checkFile(f.getAbsolutePath(), true);
+                    } else {
+                        checkFile(f.getAbsolutePath(), false);
+                    }
+                }
 
-	    } else {
-		if (args.length > 0) {
-		    if (args.length == 1 && args[0].equals("-nocon")) {
-			checkDirectory(".", true);
-		    }
-		    System.err.println("The arguments " + args + " are not valid.");
-		} else {
-		    checkDirectory(".", false);
-		}
-	    }
-	} catch (Exception e) {
-	    System.err.println("Checker handled main exception : " + e.toString());
-	}
+            } else {
+                if (args.length > 0) {
+                    if (args.length == 1 && args[0].equals("-nocon")) {
+                        checkDirectory(".", true);
+                    }
+                    System.err.println("The arguments " + args + " are not valid.");
+                } else {
+                    checkDirectory(".", false);
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Checker handled main exception : " + e.toString());
+        }
     }
 
     private static boolean validArguments(String[] arguments) {
-	return arguments.length > 0 && arguments[0].contains("-f") && !arguments[1].isEmpty();
+        return arguments.length > 0 && arguments[0].contains("-f") && !arguments[1].isEmpty();
     }
 
     private static void checkDirectory(String dir, boolean nocon) {
-	File d = new File(dir);
-	assert (d.isDirectory());
-	for (File f : d.listFiles()) {
-	    if (f.isDirectory()) {
-		checkDirectory(f.getPath(), nocon);
-	    } else if (f.getPath().endsWith(".megal")) {
-		checkFile(f.getPath(), nocon);
-	    }
-	}
+        File d = new File(dir);
+        assert (d.isDirectory());
+        for (File f : d.listFiles()) {
+            if (f.isDirectory()) {
+                checkDirectory(f.getPath(), nocon);
+            } else
+                if (f.getPath().endsWith(".megal")) {
+                    checkFile(f.getPath(), nocon);
+                }
+        }
     }
 
     private static void checkFile(String path, boolean nocon) {
-	System.out.println("Checking " + path);
-	MegaModelLoader ml = new MegaModelLoader();
-	ml.loadFile(path);
-	if (ml.getModel().getCriticalWarnings().isEmpty()) {
-	    System.out.println("  Congratulations! There are no well-formedness issues at creation time.");
-	} else
-	    return;
-	Check check = new Check(ml.getModel(), nocon);
-	if (check.getWarnings().isEmpty()) {
-	    System.out.println("  Congratulations! There are no warnings.");
-	} else {
-	    check.getWarnings().forEach(w -> System.out.println(w));
-	}
+        System.out.println("Checking " + path);
+        MegaModelLoader ml = new MegaModelLoader();
+        ml.loadFile(path);
+        if (ml.getModel().getCriticalWarnings().isEmpty()) {
+            System.out.println("  Congratulations! There are no well-formedness issues at creation time.");
+        } else
+            return;
+        Check check = new Check(ml.getModel(), nocon);
+        if (check.getWarnings().isEmpty()) {
+            System.out.println("  Congratulations! There are no warnings.");
+        } else {
+            check.getWarnings().forEach(w -> System.out.println(w));
+        }
     }
 
 }
--- checker/src/org/java/megalib/checker/services/MegalibParserListener.java
+++ checker/src/org/java/megalib/checker/services/MegalibParserListener.java
@@ -18,180 +18,198 @@ import main.antlr.techdocgrammar.MegalibParser.SubstitutionContext;
 import main.antlr.techdocgrammar.MegalibParser.SubtypeDeclarationContext;
 
 public class MegalibParserListener extends MegalibBaseListener {
-	private MegaModel model;
-	
-	public MegalibParserListener(MegaModel m){
-		model = m;
-	}
-	
-	@Override
-	public void enterSubstitution(SubstitutionContext ctx) {
-		String subject = ctx.getChild(0).getText();
-		String object = ctx.getChild(2).getText();
-		try {
-			model.addSubstitutes(subject, object);
-		} catch (WellFormednessException e) {
-			model.addWarning(e.getMessage());
-		}
-		super.enterSubstitution(ctx);
-	}
-	
-	@Override
-	public void enterSubtypeDeclaration(SubtypeDeclarationContext context) {
-		String derivedType = context.getChild(0).getText();
-		String superType = context.getChild(2).getText();
-		try {
-			model.addSubtypeOf(derivedType, superType);
-		} catch (WellFormednessException e) {
-			model.addWarning(e.getMessage());
-		}
-		if(context.children.size()==6){
-			try {
-				String link = context.getChild(5).getText();
-				model.addLink(derivedType, link.substring(1, link.length()-1));
-			} catch (WellFormednessException e) {
-				model.addWarning(e.getMessage());
-			}
-		}
-	}
-	
-	@Override
-	public void enterInstanceDeclaration(InstanceDeclarationContext context) {
-		Iterator<ParseTree> it = context.children.iterator();
-		String instance = it.next().getText();
-		//skip :
-		it.next();
-		String type = it.next().getText();
-		try {
-			model.addInstanceOf(instance, type);
-		} catch (WellFormednessException e) {
-			model.addWarning(e.getMessage());
-		}
-		while(it.hasNext()){
-			//skip TAB
-			it.next();
-			String relation = it.next().getText();
-			String object = it.next().getText();
-			try {
-				if(relation.equals("="))
-					model.addLink(instance, object.substring(1, object.length()-1));
-				else
-					model.addRelationInstances(relation, instance,object);
-			} catch (WellFormednessException e) {
-				model.addWarning(e.getMessage());
-			}
-		}
-	}
-	
-	@Override
-	public void enterRelationDeclaration(RelationDeclarationContext context) {
-		String relation = context.getChild(0).getText();
-		String type1 = context.getChild(2).getText();
-		String type2 =  context.getChild(4).getText();
-		
-		try {
-			model.addRelationDeclaration(relation, type1,type2);
-		} catch (WellFormednessException e) {
-			model.addWarning(e.getMessage());
-		}
-	}
-	
-	@Override
-	public void enterRelationInstance(RelationInstanceContext context) {
-		Iterator<ParseTree> it = context.children.iterator();
-		String subject = it.next().getText();
-		String relation = it.next().getText();
-		String object =  it.next().getText();
-		try {
-			model.addRelationInstances(relation, subject,object);
-		} catch (WellFormednessException e) {
-			model.addWarning(e.getMessage());
-		}
-		while(it.hasNext()){
-			relation = it.next().getText();
-			object = it.next().getText();
-			try {
-				if(relation.equals("="))
-					model.addLink(subject, object.substring(1, object.length()-1));
-				else
-					model.addRelationInstances(relation, subject,object);
-			} catch (WellFormednessException e) {
-				model.addWarning(e.getMessage());
-			}
-		}
-	}
-	
-	@Override
-	public void enterFunctionDeclaration(FunctionDeclarationContext context) {
-		String functionName = context.getChild(0).getText();
-		List<String> parameterTypes = new ArrayList<String>();
-		List<String> returnTypes = new ArrayList<String>();
-		
-		boolean parameter = true;
-		for (int childIndex = 2; childIndex < context.getChildCount(); childIndex++) {
-			if(!context.getChild(childIndex).getText().equals("#") && parameter == false 
-					&& !context.getChild(childIndex).getText().equals("->"))
-				returnTypes.add(context.getChild(childIndex).getText());
-			
-			if(!context.getChild(childIndex).getText().equals("#") && parameter == true 
-					&& !context.getChild(childIndex).getText().equals("->"))
-				parameterTypes.add(context.getChild(childIndex).getText());
-			
-			if(context.getChild(childIndex).getText().equals("->"))
-				parameter = false;
-		}
-		
-		try {
-			model.addFunctionDeclaration(functionName, parameterTypes,returnTypes);
-		} catch (WellFormednessException e) {
-			model.addWarning(e.getMessage());
-		}
-	}
-	
-	@Override
-	public void enterFunctionInstance(FunctionInstanceContext context) {
-		String functionName = context.getChild(0).getText();
-		List<String> outputs = new ArrayList<String>();
-		List<String> inputs = new ArrayList<String>();
-		
-		boolean parameter = true;
-		for (int childIndex = 2; childIndex < context.getChildCount(); childIndex++) {
-			if(!context.getChild(childIndex).getText().equals(",") && parameter == false 
-					&& !context.getChild(childIndex).getText().equals("|->")
-					&& !context.getChild(childIndex).getText().equals("(")
-					&& !context.getChild(childIndex).getText().equals(")") )
-				outputs.add(context.getChild(childIndex).getText());
-			
-			if(!context.getChild(childIndex).getText().equals(",") && parameter == true 
-					&& !context.getChild(childIndex).getText().equals("|->") 
-					&& !context.getChild(childIndex).getText().equals("(")
-					&& !context.getChild(childIndex).getText().equals(")") )
-				inputs.add(context.getChild(childIndex).getText());
-			
-			if(context.getChild(childIndex).getText().equals("|->"))
-				parameter = false;
-		}
-	
-		try {
-			model.addFunctionApplication(functionName, inputs,outputs);
-		} catch (WellFormednessException e) {
-			model.addWarning(e.getMessage());
-		}
-	}
-	
-	@Override
-	public void enterLink(LinkContext context) {
-		String entityname = context.getChild(0).getText();
-		String link = context.getChild(2).getText();
-		link = link.substring(1, link.length()-1);
-		try {
-			model.addLink(entityname, link);
-		} catch (WellFormednessException e) {
-			model.addWarning(e.getMessage());
-		}	
-	}
-	
-	public MegaModel getModel() {
-		return model;
-	}
+    private MegaModel model;
+
+    public MegalibParserListener(MegaModel m) {
+        model = m;
+    }
+
+    @Override
+    public void enterSubstitution(SubstitutionContext ctx) {
+        String subject = ctx.getChild(0).getText();
+        String object = ctx.getChild(2).getText();
+        try {
+            model.addSubstitutes(subject, object);
+        }
+        catch (WellFormednessException e) {
+            model.addWarning(e.getMessage());
+        }
+        super.enterSubstitution(ctx);
+    }
+
+    @Override
+    public void enterSubtypeDeclaration(SubtypeDeclarationContext context) {
+        String derivedType = context.getChild(0).getText();
+        String superType = context.getChild(2).getText();
+        try {
+            model.addSubtypeOf(derivedType, superType);
+        }
+        catch (WellFormednessException e) {
+            model.addWarning(e.getMessage());
+        }
+        if (context.children.size() == 6) {
+            try {
+                String link = context.getChild(5).getText();
+                model.addLink(derivedType, link.substring(1, link.length() - 1));
+            }
+            catch (WellFormednessException e) {
+                model.addWarning(e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void enterInstanceDeclaration(InstanceDeclarationContext context) {
+        Iterator<ParseTree> it = context.children.iterator();
+        String instance = it.next().getText();
+        it.next(); // skip colon
+        String type = it.next().getText();
+        try {
+            model.addInstanceOf(instance, type);
+        }
+        catch (WellFormednessException e) {
+            model.addWarning(e.getMessage());
+        }
+        while (it.hasNext()) {
+            it.next(); // skip TAB
+            String relation = it.next().getText();
+            String object = it.next().getText();
+            try {
+                if (relation.equals("=")) {
+                    model.addLink(instance, object.substring(1, object.length() - 1));
+                } else {
+                    model.addRelationInstances(relation, instance, object);
+                }
+            }
+            catch (WellFormednessException e) {
+                model.addWarning(e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void enterRelationDeclaration(RelationDeclarationContext context) {
+        String relation = context.getChild(0).getText();
+        String type1 = context.getChild(2).getText();
+        String type2 = context.getChild(4).getText();
+
+        try {
+            model.addRelationDeclaration(relation, type1, type2);
+        }
+        catch (WellFormednessException e) {
+            model.addWarning(e.getMessage());
+        }
+    }
+
+    @Override
+    public void enterRelationInstance(RelationInstanceContext context) {
+        Iterator<ParseTree> it = context.children.iterator();
+        String subject = it.next().getText();
+        String relation = it.next().getText();
+        String object = it.next().getText();
+        try {
+            model.addRelationInstances(relation, subject, object);
+        }
+        catch (WellFormednessException e) {
+            model.addWarning(e.getMessage());
+        }
+        while (it.hasNext()) {
+            it.next(); // skip TAB
+            relation = it.next().getText();
+            object = it.next().getText();
+            try {
+                if (relation.equals("=")) {
+                    model.addLink(subject, object.substring(1, object.length() - 1));
+                } else {
+                    model.addRelationInstances(relation, subject, object);
+                }
+            }
+            catch (WellFormednessException e) {
+                model.addWarning(e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void enterFunctionDeclaration(FunctionDeclarationContext context) {
+        String functionName = context.getChild(0).getText();
+        List<String> parameterTypes = new ArrayList<>();
+        List<String> returnTypes = new ArrayList<>();
+
+        boolean parameter = true;
+        for (int childIndex = 2; childIndex < context.getChildCount(); childIndex++) {
+            if (!context.getChild(childIndex).getText().equals("#") && parameter == false
+                && !context.getChild(childIndex).getText().equals("->")) {
+                returnTypes.add(context.getChild(childIndex).getText());
+            }
+
+            if (!context.getChild(childIndex).getText().equals("#") && parameter == true
+                && !context.getChild(childIndex).getText().equals("->")) {
+                parameterTypes.add(context.getChild(childIndex).getText());
+            }
+
+            if (context.getChild(childIndex).getText().equals("->")) {
+                parameter = false;
+            }
+        }
+
+        try {
+            model.addFunctionDeclaration(functionName, parameterTypes, returnTypes);
+        }
+        catch (WellFormednessException e) {
+            model.addWarning(e.getMessage());
+        }
+    }
+
+    @Override
+    public void enterFunctionInstance(FunctionInstanceContext context) {
+        String functionName = context.getChild(0).getText();
+        List<String> outputs = new ArrayList<>();
+        List<String> inputs = new ArrayList<>();
+
+        boolean parameter = true;
+        for (int childIndex = 2; childIndex < context.getChildCount(); childIndex++) {
+            if (!context.getChild(childIndex).getText().equals(",") && parameter == false
+                && !context.getChild(childIndex).getText().equals("|->")
+                && !context.getChild(childIndex).getText().equals("(")
+                && !context.getChild(childIndex).getText().equals(")")) {
+                outputs.add(context.getChild(childIndex).getText());
+            }
+
+            if (!context.getChild(childIndex).getText().equals(",") && parameter == true
+                && !context.getChild(childIndex).getText().equals("|->")
+                && !context.getChild(childIndex).getText().equals("(")
+                && !context.getChild(childIndex).getText().equals(")")) {
+                inputs.add(context.getChild(childIndex).getText());
+            }
+
+            if (context.getChild(childIndex).getText().equals("|->")) {
+                parameter = false;
+            }
+        }
+
+        try {
+            model.addFunctionApplication(functionName, inputs, outputs);
+        }
+        catch (WellFormednessException e) {
+            model.addWarning(e.getMessage());
+        }
+    }
+
+    @Override
+    public void enterLink(LinkContext context) {
+        String entityname = context.getChild(0).getText();
+        String link = context.getChild(2).getText();
+        link = link.substring(1, link.length() - 1);
+        try {
+            model.addLink(entityname, link);
+        }
+        catch (WellFormednessException e) {
+            model.addWarning(e.getMessage());
+        }
+    }
+
+    public MegaModel getModel() {
+        return model;
+    }
 }
