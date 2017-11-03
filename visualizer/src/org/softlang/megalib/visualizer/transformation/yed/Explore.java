f949eface294143f724aef10c71c3677e4c3d4c6
Marcel Heinz <heinz@uni-koblenz.de>
2017-11-03 14:47:27 +0100

    exploring graphml export for editing in yed.

---
+++ visualizer/src/org/softlang/megalib/visualizer/transformation/yed/Explore.java
@@ -0,0 +1,49 @@
+package org.softlang.megalib.visualizer.transformation.yed;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.jgrapht.UndirectedGraph;
+import org.jgrapht.ext.ExportException;
+import org.jgrapht.ext.GraphMLExporter;
+import org.jgrapht.ext.IntegerComponentNameProvider;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+public class Explore {
+
+	public static void main(String[] args) {
+		UndirectedGraph<String, DefaultWeightedEdge> g = new SimpleGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+
+		String v1 = "v1";
+		String v2 = "v2";
+		String v3 = "v3";
+		String v4 = "v4";
+
+		// add the vertices
+		g.addVertex(v1);
+		g.addVertex(v2);
+		g.addVertex(v3);
+		g.addVertex(v4);
+
+		// add edges to create a circuit
+		DefaultWeightedEdge e = g.addEdge(v1, v2);
+		g.
+		g.addEdge(v2, v3);
+		g.addEdge(v3, v4);
+		g.addEdge(v4, v1);
+
+		FileWriter w;
+		try {
+			GraphMLExporter<String, DefaultWeightedEdge> exporter = new GraphMLExporter<>((v) -> v, v -> "Hello", new IntegerComponentNameProvider<>(), null);
+            
+		    w = new FileWriter("test.graphml");
+		    exporter.exportGraph(g, w);
+		} catch (IOException ex) {
+		    ex.printStackTrace();
+		} catch (ExportException ex) {
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+		}
+	}
+}
