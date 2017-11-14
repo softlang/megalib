5dd15c9197384c78e7aadce1bf048cc434b03c34
Marcel Heinz <heinz@uni-koblenz.de>
2017-11-14 11:01:06 +0100

    Debugging Substitution

--- visualizer/test/org/softlang/megalib/visualizer/cli/SubstitutionDotVisualizerTest.java
+++ visualizer/test/org/softlang/megalib/visualizer/cli/SubstitutionDotVisualizerTest.java
@@ -1,7 +1,9 @@
 package org.softlang.megalib.visualizer.cli;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -18,31 +20,48 @@ import org.softlang.megalib.visualizer.models.Node;
 import org.softlang.megalib.visualizer.models.transformation.TransformerRegistry;
 import org.softlang.megalib.visualizer.transformation.dot.DOTTransformer;
 
-public class SubstitutionDemoTest {
+public class SubstitutionDotVisualizerTest {
 
 	private List<Graph> graphs;
-	private Visualizer vis;
+	private VisualizerOptions options;
 	
 	@Before
 	public void setUp() {
 		TransformerRegistry.registerTransformer("dot", (VisualizerOptions options)
 	   		     -> new DOTTransformer(options));
-	    CommandLine cli = new CommandLine(Arrays.asList("graphviz", "dot"));
+	    CommandLine cli = new CommandLine(Arrays.asList("dot"));
 
 	    String data[] = {"-f", "../checker/testsample/SubstitutionDemo/App.megal", "-t", "dot"};
 	    cli.parse(data);
-	    VisualizerOptions options = VisualizerOptions.of(cli.getRequiredArguments());
-	    vis = new Visualizer(options);
+	    options = VisualizerOptions.of(cli.getRequiredArguments());
 	    ModelToGraph mtg = new ModelToGraph(options);
 	    assertTrue(mtg.loadModel());
 	    graphs = mtg.createBlockGraphs();
-	    graphs.forEach(vis::plotGraph);
+	    assertEquals(10,graphs.size());
+	}
+	
+	@Test
+	public void testFilesExist() {
+		graphs.forEach(new Visualizer(options)::plotGraph);
+		assertTrue(new File("../output/SubstitutionDemo/App0.dot").exists());
+		assertTrue(new File("../output/SubstitutionDemo/App0.png").exists());
 	}
 
 	@Test
-	public void testGraph1() {
+	public void testGraph0() {
 		Graph g1 = graphs.get(0);
-		assertEquals("Abstract0",g1.getName());
+		assertEquals("SubstitutionDemo.BaseTechnology0",g1.getName());
+		
+		Map<String, Node> nmap = g1.getNodes();
+		assertEquals(4,nmap.size());
+		Set<Edge> edges = g1.getEdges();
+		assertEquals(4,edges.size());
+	}
+	
+	@Test
+	public void testGraph1() {
+		Graph g1 = graphs.get(1);
+		assertEquals("SubstitutionDemo.Abstract0",g1.getName());
 		
 		Map<String, Node> nmap = g1.getNodes();
 		assertEquals(5,nmap.size());
@@ -53,8 +72,8 @@ public class SubstitutionDemoTest {
 	
 	@Test
 	public void testGraph2() {
-		Graph g1 = graphs.get(1);
-		assertEquals("Abstract1",g1.getName());
+		Graph g1 = graphs.get(2);
+		assertEquals("SubstitutionDemo.Abstract1",g1.getName());
 		
 		Map<String, Node> nmap = g1.getNodes();
 		assertEquals(7,nmap.size());
@@ -65,8 +84,8 @@ public class SubstitutionDemoTest {
 	
 	@Test
 	public void testGraph4() {
-		Graph g1 = graphs.get(3);
-		assertEquals("Abstract3",g1.getName());
+		Graph g1 = graphs.get(4);
+		assertEquals("SubstitutionDemo.Abstract3",g1.getName());
 		
 		Map<String, Node> nmap = g1.getNodes();
 		assertEquals(8,nmap.size());
@@ -76,20 +95,9 @@ public class SubstitutionDemoTest {
 	}
 	
 	@Test
-	public void testGraph5() {
-		Graph g1 = graphs.get(4);
-		assertEquals("BaseTechnology0",g1.getName());
-		
-		Map<String, Node> nmap = g1.getNodes();
-		assertEquals(4,nmap.size());
-		Set<Edge> edges = g1.getEdges();
-		assertEquals(4,edges.size());
-	}
-	
-	@Test
 	public void testGraph6() {
 		Graph g1 = graphs.get(5);
-		assertEquals("Technology0",g1.getName());
+		assertEquals("SubstitutionDemo.Technology0",g1.getName());
 		
 		Map<String, Node> nmap = g1.getNodes();
 		assertEquals(15,nmap.size());
