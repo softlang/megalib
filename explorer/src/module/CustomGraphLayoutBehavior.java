9bdda349002e6e3bb87e594d59f6ca6a4a05f931
Unknown <dremagen32@gmail.com>
2018-09-28 12:38:04 +0200

    scale graph layout with graph size

--- explorer/src/module/CustomGraphLayoutBehavior.java
+++ explorer/src/module/CustomGraphLayoutBehavior.java
@@ -1,24 +1,19 @@
 package module;
 
+import org.eclipse.gef.geometry.planar.Dimension;
+import org.eclipse.gef.geometry.planar.Point;
 import org.eclipse.gef.geometry.planar.Rectangle;
+import org.eclipse.gef.graph.Graph;
+import org.eclipse.gef.graph.Node;
 import org.eclipse.gef.layout.LayoutContext;
 import org.eclipse.gef.zest.fx.behaviors.GraphLayoutBehavior;
 
 public class CustomGraphLayoutBehavior extends GraphLayoutBehavior{
 	
-	protected Rectangle computeLayoutBounds() {
-		
-		LayoutContext layoutContext = getLayoutContext();
-				
-		Rectangle newBounds = new Rectangle();			
-			/* TODO:
-			 * Find a way to calculate appropiate size
-			 * according to graph's nodes and edges
-			 * 
-				x = layoutContext.getNodes();
-				y = layoutContext.getEdges();
-			*/
-			newBounds = new Rectangle(0, 0, 1000, 1000);
+	protected Rectangle computeLayoutBounds() {			
+		Rectangle newBounds;			
+		Graph g = (Graph) getHost().getAdaptable().getContents().get(0);
+		newBounds = new Rectangle(0, 0, g.getNodes().size()*100, g.getNodes().size()*80);
 		return newBounds;
 	}
 
