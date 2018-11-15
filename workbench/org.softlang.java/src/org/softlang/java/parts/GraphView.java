1741189ebbff632520a54662519a55d29a138f4c
Andre Emmerichs <aemmerichs@uni-koblenz.de>
2018-11-15 15:41:55 +0100

    [Workbench] Preperations to use GSON to read JSON

--- workbench/org.softlang.java/src/org/softlang/java/parts/GraphView.java
+++ workbench/org.softlang.java/src/org/softlang/java/parts/GraphView.java
@@ -9,19 +9,25 @@ import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+import org.apache.commons.io.FileUtils;
 import org.eclipse.gef.graph.Graph;
-import org.eclipse.gef.graph.Node;
-import org.eclipse.gef.zest.fx.ZestProperties;
 import org.eclipse.gef.zest.fx.ui.parts.ZestFxUiView;
 import org.eclipse.ui.part.IShowInTarget;
 import org.eclipse.ui.part.ShowInContext;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.event.EventHandler;
+
 
 @SuppressWarnings("restriction")
 public class GraphView extends ZestFxUiView implements IShowInTarget {
@@ -39,7 +45,7 @@ public class GraphView extends ZestFxUiView implements IShowInTarget {
 			File f = (File) in;
 			fname = f.getName();
 			setWatchService(f);
-			Graph g = createGraphFromJson(f);
+			Graph g = createGraphFromJson(f.toPath());
 			setGraph(g);
 			return true;
 		}
@@ -87,7 +93,38 @@ public class GraphView extends ZestFxUiView implements IShowInTarget {
 		new Thread(task).start();
 	}
 
-	private Graph createGraphFromJson(File f) {
+	private Graph createGraphFromJson(Path f) {
+		String json;
+		try {
+			LinkedList<String> urls = null;
+			LinkedList<String> bindings = null;
+			
+			json = FileUtils.readFileToString(f.toFile());
+			JsonElement root = new JsonParser().parse(json);
+			JsonArray nodes =  (JsonArray) root.getAsJsonObject().get("Nodes");
+			JsonArray edges = (JsonArray) root.getAsJsonObject().get("Edges");
+			for(JsonElement j: nodes) {
+				String name = j.getAsJsonObject().get("name").getAsString();
+				if(j.getAsJsonObject().has("links")) {
+					urls = new LinkedList();
+					for(JsonElement u: j.getAsJsonObject().get("links").getAsJsonArray()) {
+					urls.add(u.getAsString());
+					}
+				}
+				if(j.getAsJsonObject().has("bindings")) {
+					bindings = new LinkedList();
+					for(JsonElement b: j.getAsJsonObject().get("bindings").getAsJsonArray()) {
+					bindings.add(b.getAsString());
+					}
+				}
+			//TODO: Add function which creates Nodes according to above data
+			//e.g. createNode(name, links, bindings)
+			//TODO: Same for edges
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
 		return null;
 	}
 
