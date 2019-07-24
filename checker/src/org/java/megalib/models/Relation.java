bf3c4a59c04fa4259c9f540b6505de7eae9c3a71
Marcel Heinz <heinz@uni-koblenz.de>
2019-07-24 12:49:38 +0200

    Import Graph is accessible via model.getImportGraph

--- checker/src/org/java/megalib/models/Relation.java
+++ checker/src/org/java/megalib/models/Relation.java
@@ -4,33 +4,38 @@ public class Relation {
 	private String subject;
 	private String object;
 	private Block block;
-	
+
 	public Relation(String s, String o){
 		subject = s;
 		object = o;
 	}
-	
+
 	public String getSubject(){
 		return subject;
 	}
-	
+
 	public String getObject(){
 		return object;
 	}
-	
+
 	public Block getBlock(){
 		return block;
 	}
-	
+
 	public void setBlock(Block b){
 		block = b;
 	}
-	
+
 	public String getModule(){
 		return block.getModule();
 	}
-	
+
 	@Override
+    public String toString() {
+        return subject + " R " + object;
+    }
+
+    @Override
 	public boolean equals(Object obj) {
 		if ( this == obj )
 		      return true;
@@ -41,7 +46,7 @@ public class Relation {
 		return subject.equals(((Relation) obj).getSubject())
 				&& object.equals(((Relation) obj).getObject());
 	}
-	
+
 	@Override
 	public int hashCode(){
 		return (subject+object).hashCode();
--- checker/test/org/java/megalib/checker/services/ImportTest.java
+++ checker/test/org/java/megalib/checker/services/ImportTest.java
@@ -1,10 +1,12 @@
 package org.java.megalib.checker.services;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Set;
 
 import org.java.megalib.models.Block;
@@ -16,7 +18,7 @@ import org.junit.Test;
 public class ImportTest {
 
     private ModelLoader ml;
-	private MegaModel model;
+    private MegaModel model;
 
     @Before
     public void setUp() throws IOException {
@@ -38,6 +40,16 @@ public class ImportTest {
     }
 
     @Test
+    public void testImportGraph() {
+        List<Relation> ig = model.getImportGraph();
+        assertFalse(ig.isEmpty());
+        ig.contains(new Relation("bcd.d.D", "bcd.C"));
+        ig.contains(new Relation("bcd.C", "A"));
+        ig.contains(new Relation("bcd.C", "bcd.B"));
+        ig.contains(new Relation("bcd.B", "A"));
+    }
+
+    @Test
     public void testContent() {
         assertTrue(model.isInstanceOf("?spec", "Artifact"));
         assertTrue(model.isInstanceOf("?a1", "Artifact"));
@@ -47,38 +59,39 @@ public class ImportTest {
 
     @Test
     public void testBlockWisdom() {
-	    	//every model's relationship knows its block and every block knows subject and object
-	    	for(Set<Relation>rset : model.getRelationships().values()) {
-	    		for(Relation r : rset) {
-	    			if(model.getSubtypesMap().containsKey(r.getSubject())
-	        				||model.getRelationshipDeclarationMap().containsKey(r.getSubject())) {
-	        			continue;
-	    			}
-	        		Block b = r.getBlock();
-	        		assertNotNull(b);
-	        		assertNotNull(b.getInstanceOfMap().get(r.getSubject()));
-	        		assertNotNull(b.getInstanceOfMap().get(r.getObject()));
-	        	}
-	    	}
-	    	
-	    	for(Block b : model.getBlocks()) {
-	    		for(Set<Relation> rset : b.getRelationships().values()) {
-	    			for(Relation r : rset) {
-	    				if(model.getSubtypesMap().containsKey(r.getSubject())) {
-	    					System.err.println(r.getSubject()+"---"+r.getObject());
-	    				}
-	    				assertNotNull(b.getInstanceOfMap().get(r.getSubject()));
-	    				assertNotNull(b.getInstanceOfMap().get(r.getObject()));
-	    			}
-	    		}
-	    	}
+        // every model's relationship knows its block and every block knows
+        // subject and object
+        for(Set<Relation> rset : model.getRelationships().values()){
+            for(Relation r : rset){
+                if(model.getSubtypesMap().containsKey(r.getSubject())
+                   || model.getRelationshipDeclarationMap().containsKey(r.getSubject())){
+                    continue;
+                }
+                Block b = r.getBlock();
+                assertNotNull(b);
+                assertNotNull(b.getInstanceOfMap().get(r.getSubject()));
+                assertNotNull(b.getInstanceOfMap().get(r.getObject()));
+            }
+        }
+
+        for(Block b : model.getBlocks()){
+            for(Set<Relation> rset : b.getRelationships().values()){
+                for(Relation r : rset){
+                    if(model.getSubtypesMap().containsKey(r.getSubject())){
+                        System.err.println(r.getSubject() + "---" + r.getObject());
+                    }
+                    assertNotNull(b.getInstanceOfMap().get(r.getSubject()));
+                    assertNotNull(b.getInstanceOfMap().get(r.getObject()));
+                }
+            }
+        }
     }
-    
+
     @Test
     public void testMissingModule() throws IOException {
-    		ml = new ModelLoader();
+        ml = new ModelLoader();
         ml.loadFile("testsample/ImportDemo/F.megal");
-        assertEquals("Error Missing import target: from F to E",ml.getTypeErrors().get(0));
+        assertEquals("Error Missing import target: from F to E", ml.getTypeErrors().get(0));
     }
 
 }
